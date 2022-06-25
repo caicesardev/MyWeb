d9430aecad6bb074b1b47f309a3dec662a35c9b6
Caio Gomes <caicesardev@gmail.com>
2022-06-25 04:06:19 +0200

    modfied settings

--- my_web/my_web/settings.py
+++ my_web/my_web/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'portfolio.apps.PortfolioConfig',
+    'my_web.portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
