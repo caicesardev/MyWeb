81825c877c6afab43fc28df5e41e14646a354418
Caio Gomes <caicesardev@gmail.com>
2022-06-25 04:16:08 +0200

    updated settings.py

--- my_web/my_web/urls.py
+++ my_web/my_web/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from my_web import portfolio
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
