4a78abdc2be822dabc4caa8be1908d5c3e094e78
Caio Gomes <caicesardev@gmail.com>
2022-06-25 03:29:50 +0200

    removed django-environ

--- my_web/my_web/settings.py
+++ my_web/my_web/settings.py
@@ -11,14 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-import environ
 from pathlib import Path
-
-# Initialise environment variables
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
